c4ee34b0a43ef085b33c2117532f4c115f76a907
vezzo95 <vezzo.vz.95@gmail.com>
2018-02-23 14:29:14 +0100

    Update sa-matching.c

--- sa-matching/sa-matching.c
+++ sa-matching/sa-matching.c
@@ -18,11 +18,15 @@
 */
 
 #include "sa-matching.h"
-#include <time.h>
+
 KSEQ_INIT(gzFile, gzread)
 
 static inline int min(int a, int b) {
         return a < b ? a : b;
+}
+
+static inline int mid(int a, int b) {
+        return a + (b - a) / 2;
 }
 
 /* compares a pattern (a string) and a suffix of the text, by extracting the prefix of
@@ -58,9 +62,52 @@ static char* read_text(char* filename) {
 /* returns the position of the suffix array corresponding to a match,
    returns -1 if no such match exists
 */
+
+static unsigned int*
+lcp_kasai(const char* text, const int* sa, const unsigned int n) {
+        //lcp-array allocation
+        unsigned int* lcp = (unsigned int*) malloc(n * sizeof(unsigned int));
+        if(lcp == NULL)
+                return lcp;
+
+        //inverted-suffix-array allocation
+        unsigned int* i_sa = (unsigned int*) malloc(n * sizeof(unsigned int));
+        if(i_sa == NULL) {
+                free(lcp);
+                return NULL;
+        }
+        
+        //inverted suffix array filling: 
+        //Memorize, for each i-suffix of the string, his rank in suffix array
+        for(unsigned int i = 0; i < n; i++)
+                i_sa[sa[i]] = i;
+        
+        unsigned int l = 0;
+        unsigned int k, j;
+        for(unsigned int i = 0; i < n - 1; i++) {
+                k = i_sa[i];    
+                j = sa[k - 1];
+                /* 
+                i = sa[k]
+                j = sa[k - 1]
+                */
+                while(text[i + l] == text[j + l])
+                        l++;
+                lcp[k] = l;
+                if(l > 0)
+                        l = l - 1;
+        }
+        //lcp[0] is always equals to 0
+        lcp[0] = 0;
+        
+        //inverted-suffix-array memory cleaning
+        free(i_sa);
+        return lcp;
+}
+
 static unsigned int
 sa_search(const char* text, const int* sa, const unsigned int n,
-          const unsigned int* lcp, const char* pattern, const unsigned int m) {
+          const unsigned int* lcp, const unsigned int* lcplr, const unsigned int o, const char* pattern, const unsigned int m) {
         unsigned int found = -1;
         if (suffix_cmp(text, sa[0], n, pattern, m) < 0 ||
             suffix_cmp(text, sa[n - 1], n, pattern, m) > 0)
@@ -95,7 +142,7 @@ lcp2(const char* s1, const char* s2, const unsigned int len) {
 
 static unsigned int
 sa_search_1(const char* text, const int* sa, const unsigned int n,
-            const unsigned int* lcp, const char* pattern, const unsigned int m) {
+          const unsigned int* lcp, const unsigned int* lcplr, const unsigned int o, const char* pattern, const unsigned int m) {
         unsigned int found = -1;
         if (suffix_cmp(text, sa[0], n, pattern, m) < 0 ||
             suffix_cmp(text, sa[n - 1], n, pattern, m) > 0)
@@ -129,8 +176,8 @@ sa_search_1(const char* text, const int* sa, const unsigned int n,
 
 /* Accelerant #2 */
 static unsigned int
-sa_search_2(const char* text, const unsigned int* sa, const unsigned int n,
-            const unsigned int* lcp, const char* pattern, const unsigned int m) {
+sa_search_2(const char* text, const int* sa, const unsigned int n,
+          const unsigned int* lcp, const unsigned int* lcplr, const unsigned int o, const char* pattern, const unsigned int m) {
         unsigned int found = -1;
         if (suffix_cmp(text, sa[0], n, pattern, m) < 0 ||
             suffix_cmp(text, sa[n - 1], n, pattern, m) > 0)
@@ -221,46 +268,66 @@ sa_search_2(const char* text, const unsigned int* sa, const unsigned int n,
         return found;
 }
 
-static unsigned int*
-lcp_kasai(const char* text, const unsigned int* sa, const unsigned int n) {
-        //lcp-array allocation
-        unsigned int* lcp = (unsigned int*) malloc(n * sizeof(unsigned int));
-        if(lcp == NULL)
-                return lcp;
+/* Accelerant #3 */
+static unsigned int
+lcplr_compute(const unsigned int* lcp, const unsigned int ss, const unsigned int se,
+              unsigned int* lcplr, const unsigned int si) {
+        /* lcplr is a segment tree
+           ss is a segment start
+           se is a segment end
+           si is the lcplr memory cell that I want compute */
+        if(ss == se) {
+                lcplr[si] = lcp[ss];
+                return lcplr[si];
+        }
+                
+        int m = mid(ss, se);
+        lcplr[si] = min(lcplr_compute(lcp, ss, m, lcplr, 2 * si + 1),
+                        lcplr_compute(lcp, m + 1, se, lcplr, 2 * si + 2));
+        return lcplr[si];        
+}
 
-        //inverted-suffix-array allocation
-        unsigned int* i_sa = (unsigned int*) malloc(n * sizeof(unsigned int));
-        if(i_sa == NULL) {
-                free(lcp);
-                return NULL;
-        }
-        
-        //inverted suffix array: sa[i] = j, i_sa[j] = i
-        for(unsigned int i = 0; i < n; i++)
-                i_sa[sa[i]] = i;
-        
-        unsigned int l = 0;
-        unsigned int k, j;
-        for(unsigned int i = 0; i < n - 1; i++) {
-                k = i_sa[i];    //sa[k] == i
-                j = sa[k - 1]; //i_sa[j] == k-1
-                while(text[i+l] == text[j + l])
-                        l++;
-                lcp[k] = l;
-                if(l > 0)
-                        l = l - 1;
-        }
-        //lcp[0] is always equals to 0
-        lcp[0] = 0;
-        
-        //inverted-suffix-array memory cleaning
-        free(i_sa);
-        return lcp;
+static unsigned int*
+lcplr_build(const unsigned int* lcp, const unsigned int n, const unsigned int o) {
+        unsigned int* lcplr = (unsigned int*) malloc(o * sizeof(unsigned int));
+        lcplr_compute(lcp, 0, n-1, lcplr, 0);
+        return lcplr;
 }
 
 static unsigned int
-sa_search_3(const char* text, const int* sa, const unsigned int n, const unsigned int* lcp, 
-            const char* pattern, const unsigned int m) {
+rmq_utils(const unsigned int* lcplr, const unsigned int ss, const unsigned int se, 
+    const unsigned int qs, const unsigned int qe, const unsigned int index) {
+        /* lcplr is a segment tree
+           ss is a segment start
+           se is a segment end
+           qs is the query interval start
+           qe is the query interval end
+           index is the lcplr memory cell that I'm currently watching in */
+        
+        //if segment ss to se is part of query interval
+        if(qs <= ss && qe >= se)
+                return lcplr[index];
+        
+        //if segment ss to se is outside query interval
+        if(se < qs || ss > qe)
+                return UINT_MAX;
+        
+        //if part of the segment overlap with query interval
+        unsigned int m = mid(ss, se);
+        return min(rmq_utils(lcplr, ss, m, qs, qe, 2 * index + 1),
+                   rmq_utils(lcplr, m + 1, se, qs, qe, 2 * index + 2));
+}
+
+static unsigned int
+rmq(const unsigned int* lcplr, const unsigned int n, const unsigned int qs, const unsigned int qe) {
+        if(qs < 0 || qe > n - 1 || qs > qe)
+                return 0;
+        return rmq_utils(lcplr, 0, n-1, qs, qe, 0);
+}
+
+static unsigned int
+sa_search_3(const char* text, const int* sa, const unsigned int n,
+          const unsigned int* lcp, const unsigned int* lcplr, const unsigned int o, const char* pattern, const unsigned int m) {
         unsigned int found = -1;
         if (suffix_cmp(text, sa[0], n, pattern, m) < 0 ||
             suffix_cmp(text, sa[n - 1], n, pattern, m) > 0)
@@ -272,22 +339,13 @@ sa_search_3(const char* text, const int* sa, const unsigned int n, const unsigne
         unsigned int rp = n - 1;
         unsigned int l = lcp2(text + sa[lp], pattern, m);
         unsigned int r = lcp2(text + sa[rp], pattern, m);
-        unsigned int
-                lcp_t(const unsigned int p1, const unsigned int p2) {
-                unsigned int min = lcp[p1];
-                if(p2 == p1 + 1)
-                        return lcp[p2];
-                for(int k = p1; k < p2; k++) 
-                        if(lcp[k] < min)
-                                min = lcp[k];
-                return min;
-        }
+
         while (lp < rp - 1) {
                 unsigned int mp = lp + (rp - lp) / 2;
                 if (l > r)
-                        if (lcp_t(lp, mp) > l) {
+                        if (rmq(lcplr, n, lp, mp) > l) {
                                 lp = mp;
-                        } else if (lcp_t(lp, mp) < l) {
+                        } else if (rmq(lcplr, n, lp, mp) < l) {
                                 rp = mp;
                                 r += lcp2(text + sa[rp] + r, pattern + r, min(m - r, n - r));
                         } else {
@@ -306,9 +364,9 @@ sa_search_3(const char* text, const int* sa, const unsigned int n, const unsigne
                                 }
                         }
                 else if (l < r)
-                        if (lcp_t(rp, mp) > r) {
+                        if (rmq(lcplr, n, rp, mp) > r) {
                                 rp = mp;
-                        } else if (lcp_t(rp, mp) < r) {
+                        } else if (rmq(lcplr, n, rp, mp) < r) {
                                 lp = mp;
                                 l += lcp2(text + sa[lp] + r, pattern + l, min(m - l, n - l));
                         } else {
@@ -328,9 +386,9 @@ sa_search_3(const char* text, const int* sa, const unsigned int n, const unsigne
                         }
                 else {
                         /* l == r */
-                        if (lcp_t(lp, rp) > l) {
+                        if (rmq(lcplr, n, lp, rp) > l) {
                                 lp = mp;
-                        } else if (lcp_t(rp, lp) > r) {
+                        } else if (rmq(lcplr, n, rp, lp) > r) {
                                 rp = mp;
                         } else {
                                 /* lcp_t(mp, rp) == lcp_t(lp, mp) == l */
@@ -348,7 +406,7 @@ sa_search_3(const char* text, const int* sa, const unsigned int n, const unsigne
                                 }
                         }
                 }
-        }   
+        }
         if (found == -1) {
                 if (suffix_cmp(text, sa[lp], n, pattern, m) == 0) found = lp;
                 if (suffix_cmp(text, sa[rp], n, pattern, m) == 0) found = rp;
@@ -364,8 +422,8 @@ int main(int argc, char **argv) {
         uint32_t n = strlen(text);
         uint32_t m = strlen(pattern);
 
-        unsigned int (*search_f)(const char*, const int*, const unsigned int,
-                                 const unsigned int*, const char*, const unsigned int);
+        unsigned int (*search_f)(const char*, const int*, const unsigned int, const unsigned int*,
+                                 const unsigned int*, const unsigned int, const char*, const unsigned int);
 
         printf("Accelerant type %d\n", args_info.accelerant_arg);
         if (args_info.accelerant_arg == 1)
@@ -375,25 +433,26 @@ int main(int argc, char **argv) {
         else if (args_info.accelerant_arg == 3)
                 search_f = sa_search_3;
         else
-                search_f = sa_search;
+                search_f = sa_search; 
 
 
 
         int* sa = malloc(n * sizeof(*sa));
         //unsigned int* lcp = malloc(n * sizeof(*sa));
-
         if(sais((unsigned char*) text, sa, (int)n) != 0) {
                 fprintf(stderr, "%s: Cannot allocate memory.\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
-
+        //sais_lcp((unsigned char*) text, sa, lcp, n);
+        //lcp[0] = 0;
         unsigned int* lcp = lcp_kasai(text, sa, n);
-        unsigned int found = search_f(text, sa, n, lcp, pattern, m);
+        unsigned int o = 2 * pow(2, log2(n)) - 1;
+        unsigned int* lcplr = lcplr_build(lcp, n, o);
+        unsigned int found = search_f(text, sa, n, lcp, lcplr, o, pattern, m);
 
         /* If found != -1 we have found at least one occurrence.
            Let's find all of them!
         */
-
         if (found != -1) {
                 unsigned int lp = found;
                 for (;lcp[lp] >= m; lp--) {}
